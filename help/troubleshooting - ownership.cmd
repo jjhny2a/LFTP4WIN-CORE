4d6f582a95995b2b2783368205b21dff052b570b
userdocs <16525024+userdocs@users.noreply.github.com>
2020-02-12 20:48:19 +0000

    tweak - fix file ownership and permissions.

--- help/troubleshooting - ownership.cmd
+++ help/troubleshooting - ownership.cmd
@@ -1,1 +1,11 @@
-takeown /f "%cd%\..\downloads" /r & icacls "%cd%\..\downloads" /reset /t /c /q
+@echo off
+:: Move to the LFTP4WIN root directory
+PUSHD "%~dp0.."
+:: If this fails then exit
+If %errorlevel% NEQ 0 goto:eof
+:: Set the path to a variable.
+set FIX_PERMS_PATH="%CD%"
+:: Take ownership and reset permissions
+takeown /f "%FIX_PERMS_PATH%" /r & icacls "%FIX_PERMS_PATH%" /reset /t /c /q
+:: Press enter to exit th script
+pause
